ca8a1cc59fee027d8046c3da598c83cbc94b3adf
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-16 15:46:10 -0300

    passando no primeiro test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then( response => {
+      setRepositories(response.data);
+    })
+  }, []); 
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:'Gustavo Gomes',
+      url:'https://github.com/GustaGomes/gostack-template-conceitos-reactjs',
+      techs: ['Node.js', 'ReactJS']
+    })
+
+    setRepositories([ ... repositories, response.data ]);
+    // assim eu vou copiar todos os  'repositories' que eu ja tenho e adiciono ele no final do array de repositorios 'response.data'
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +30,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+              <li key={repository.id}>
+              {repository.title}
+    
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
